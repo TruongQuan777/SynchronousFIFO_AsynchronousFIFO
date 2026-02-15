b9d7779fcc2c59be643f88a0149f1679614cc84f
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-15 22:17:38 +0800

    Update b2g_converter.v

--- AsynchronousFIFO/b2g_converter.v
+++ AsynchronousFIFO/b2g_converter.v
@@ -1,6 +1,6 @@
 module b2g_converter #(parameter DATA_WIDTH=8, parameter PTR_WIDTH=$clog2(DATA_WIDTH))(
-    input  wire [WIDTH-1:0] binary_in,
-    output wire [WIDTH-1:0] gray_out
+    input  wire [PTR_WIDTH:0] binary_in,
+    output wire [PTR_WIDTH:0] gray_out
 );
 
     // The Gray code is calculated by XORing the binary 
