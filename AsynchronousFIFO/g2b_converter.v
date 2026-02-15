376a9d3ef00fc37888cf8fb9ee65e2aab036bf13
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-15 17:44:54 +0800

    Create g2b_converter.v

---
+++ AsynchronousFIFO/g2b_converter.v
@@ -0,0 +1,14 @@
+module g2b_converter #(DATA_WIDTH=8, PTR_WIDTH=$clog2(DATA_WIDTH))(
+  input  wire [DATA_WIDTH-1:0] gray_in,
+  output reg  [DATA_WIDTH-1:0] binary_out
+);
+    integer i;
+    always @(*) 
+      begin
+        binary_out[DATA_WIDTH-1] = gray_in[DATA_WIDTH-1];
+        for (i = DATA_WIDTH-2; i >= 0; i = i - 1) 
+          begin
+            binary_out[i] = binary_out[i+1] ^ gray_in[i];
+          end
+      end
+endmodule
