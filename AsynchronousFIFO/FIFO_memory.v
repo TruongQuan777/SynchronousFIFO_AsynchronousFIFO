e604f3cf6b9d9350bb0cf5fd17b253344aeba8ec
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-15 22:31:15 +0800

    Update FIFO_memory.v

--- AsynchronousFIFO/FIFO_memory.v
+++ AsynchronousFIFO/FIFO_memory.v
@@ -1,4 +1,4 @@
-module FIFO #(parameter DATA_WIDTH=8, parameter FIFO_WIDTH=8, parameter PTR_WIDTH=$clog2(DATA_WIDTH))(
+module FIFO #(parameter DATA_WIDTH=8, parameter FIFO_WIDTH=8, parameter PTR_WIDTH=$clog2(FIFO_WIDTH))(
   input[DATA_WIDTH-1:0] wdata,
   input[PTR_WIDTH:0] wptr,rptr,
   input wclk,w_en,full
