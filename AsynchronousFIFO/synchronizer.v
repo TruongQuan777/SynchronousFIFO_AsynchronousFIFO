ba41978fed6dc980740e0a2af3ce62a0f001e1d2
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-15 17:45:55 +0800

    Update synchronizer.v

--- AsynchronousFIFO/synchronizer.v
+++ AsynchronousFIFO/synchronizer.v
@@ -1,15 +1,19 @@
 module synchronizer #(DATA_WIDTH=8, PTR_WIDTH=$clog2(DATA_WIDTH)) (
-input clk, rst_n, [WIDTH:0] d_in, output reg [WIDTH:0] d_out
+  input clk, rst_n, [DATA-WIDTH:0] d_in, 
+  output reg [DATA_WIDTH:0] d_out
 );
   reg [WIDTH:0] q1;
-  always@(posedge clk) begin
-    if(!rst_n) begin
-      q1 <= 0;
-      d_out <= 0;
-    end
-    else begin
-      q1 <= d_in;
-      d_out <= q1;
+  always@(posedge clk) 
+    begin
+      if(!rst_n) 
+        begin
+        q1 <= 0;
+        d_out <= 0;
+        end
+      else 
+        begin
+        q1 <= d_in;
+        d_out <= q1;
     end
   end
 endmodule
