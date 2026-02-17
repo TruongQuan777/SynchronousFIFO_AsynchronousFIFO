c80dae28642dcd47537e02c74bb9d166dcb07139
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-18 00:09:23 +0800

    Update async_fifo.v

--- AsynchronousFIFO/async_fifo.v
+++ AsynchronousFIFO/async_fifo.v
@@ -4,25 +4,21 @@
 `include "fifo_mem.v"
 
 module async_fifo #(parameter DEPTH=8, DATA_WIDTH=8, parameter PTR_WIDTH = $clog2(DEPTH)) (
-  input wclk, wrst_n,
-  input rclk, rrst_n,
+  input w_clk, wrst_n,
+  input r_clk, rrst_n,
   input w_en, r_en,
   input [DATA_WIDTH-1:0] data_in,
   output reg [DATA_WIDTH-1:0] data_out,
   output reg full, empty
 );
- 
-  reg [PTR_WIDTH:0] g_wptr_sync, g_rptr_sync;
-  reg [PTR_WIDTH:0] b_wptr, b_rptr;
-  reg [PTR_WIDTH:0] g_wptr, g_rptr;
-
-  wire [PTR_WIDTH-1:0] waddr, raddr;
+  reg [PTR_WIDTH:0] b_w_ptr, g_w_ptr, g_w_ptr_sync, b_w_ptr_sync;
+  reg [PTR_WIDTH:0] b_r_ptr, g_r_ptr, g_r_ptr_sync, b_r_ptr_sync;
 
   synchronizer #(PTR_WIDTH) sync_wptr (rclk, rrst_n, g_wptr, g_wptr_sync); //write pointer to read clock domain
   synchronizer #(PTR_WIDTH) sync_rptr (wclk, wrst_n, g_rptr, g_rptr_sync); //read pointer to write clock domain 
   
-  wptr_handler #(PTR_WIDTH) wptr_h(wclk, wrst_n, w_en,g_rptr_sync,b_wptr,g_wptr,full);
-  rptr_handler #(PTR_WIDTH) rptr_h(rclk, rrst_n, r_en,g_wptr_sync,b_rptr,g_rptr,empty);
-  fifo_mem fifom(wclk, w_en, rclk, r_en,b_wptr, b_rptr, data_in,full,empty, data_out);
+  wptr_handler #(PTR_WIDTH) wptr_h(w_clk, wrst_n, w_en,b_r_ptr_sync,b_w_ptr,full);
+  rptr_handler #(PTR_WIDTH) rptr_h(r_clk, rrst_n, r_en,b_w_ptr_sync,b_r_ptr, empty);
+  fifo_mem fifom(data_in, b_w_ptr,b_r_ptr, w_clk, w_en, full, r_clk,r_en,empty);
 
 endmodule
