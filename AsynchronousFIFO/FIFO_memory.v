df5b2e161272a45b0964e688aef232e5fb97137d
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-17 22:33:31 +0800

    Update FIFO_memory.v

--- AsynchronousFIFO/FIFO_memory.v
+++ AsynchronousFIFO/FIFO_memory.v
@@ -1,22 +1,22 @@
-module FIFO #(parameter DATA_WIDTH=8, parameter FIFO_WIDTH=8, parameter PTR_WIDTH=$clog2(FIFO_WIDTH))(
+module fifo_mem #(parameter DATA_WIDTH=8, parameter FIFO_WIDTH=8, parameter PTR_WIDTH=$clog2(FIFO_WIDTH))(
   input[DATA_WIDTH-1:0] data_in,
-  input[PTR_WIDTH:0] wptr,rptr,
-  input wclk,w_en,full
-  input rclk,r_en,empty,
-  output rdata
+  input[PTR_WIDTH:0] w_ptr,r_ptr,
+  input w_clk,w_en,full
+  input r_clk,r_en,empty,
+  output data_out
 );
   reg[DATA_WIDTH-1:0] mem[FIFO_WIDTH-1:0]
-  always@(posedge wclk) begin
+  always@(posedge w_clk) begin
     if(w_en & !full) begin
-      mem[wptr[PTR_WIDTH-1:0]] <= data_in;
+      mem[w_ptr[PTR_WIDTH-1:0]] <= data_in;
     end
   end
   /*
-  always@(posedge rclk) begin
+  always@(posedge r_clk) begin
     if(r_en & !empty) begin
-      data_out <= mem[rptr[PTR_WIDTH-1:0]];
+      data_out <= mem[r_ptr[PTR_WIDTH-1:0]];
     end
   end
   */
-  assign data_out = mem[rptr[PTR_WIDTH-1:0]];
+  assign data_out = mem[r_ptr[PTR_WIDTH-1:0]];
 endmodule
