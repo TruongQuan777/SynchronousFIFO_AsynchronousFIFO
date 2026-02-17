bf901588795cfebacf113f601906325128ea0446
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-17 23:25:35 +0800

    Update rptr_handler.v

--- AsynchronousFIFO/rptr_handler.v
+++ AsynchronousFIFO/rptr_handler.v
@@ -1,24 +1,24 @@
 module rptr_handler #(parameter DATA_WIDTH=8, parameter FIFO_WIDTH=8, parameter PTR_WIDTH=$clog2(FIFO_WIDTH))(
-  input rclk, rrst_n, r_en,
-  input [PTR_WIDTH:0] wptr_sync,
-  output reg [PTR_WIDTH:0] rptr,
+  input r_clk, rrst_n, r_en,
+  input [PTR_WIDTH:0] w_ptr_sync,
+  output reg [PTR_WIDTH:0] r_ptr,
   output reg empty
 );
   
-  always@(posedge rclk or negedge rrst_n) begin
+  always@(posedge r_clk or negedge rrst_n) begin
     if(!rrst_n) 
       begin
-        b_rptr <= 0;
+        r_ptr <= 0;
       end
     else 
       begin
-        b_rptr <= b_rptr+(r_en & !empty);;
+        r_ptr <= r_ptr+(r_en & !empty);;
       end
   end
   
-  always@(posedge rclk or negedge rrst_n) 
+  always@(posedge r_clk or negedge rrst_n) 
     begin
       if(!rrst_n) empty <= 1;
-      else        empty <= b_wptr_sync == b_rptr+(r_en & !empty);
+      else        empty <= w_ptr_sync == r_ptr+(r_en & !empty);
     end
 endmodule
