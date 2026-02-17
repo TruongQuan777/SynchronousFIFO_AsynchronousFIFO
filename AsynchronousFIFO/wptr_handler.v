1c8e18c064ca8f785726709e7a2fdfbb58854910
Quan777 <106910746+TruongQuan777@users.noreply.github.com>
2026-02-17 23:29:03 +0800

    Update wptr_handler.v

--- AsynchronousFIFO/wptr_handler.v
+++ AsynchronousFIFO/wptr_handler.v
@@ -1,21 +1,21 @@
 module wptr_handler #(parameter DATA_WIDTH=8, parameter FIFO_WIDTH=8, parameter PTR_WIDTH=$clog2(FIFO_WIDTH)) (
-  input wclk, wrst_n, w_en,
-  input [PTR_WIDTH:0] rptr_sync,
-  output reg [PTR_WIDTH:0] wptr,
+  input w_clk, wrst_n, w_en,
+  input [PTR_WIDTH:0] r_ptr_sync,
+  output reg [PTR_WIDTH:0] w_ptr,
   output reg full
 );
 
   
-  always@(posedge wclk or negedge wrst_n) begin
+  always@(posedge w_clk or negedge wrst_n) begin
     if(!wrst_n) begin
-      wptr <= 0; // set default value
+      w_ptr <= 0; // set default value
     end
     else begin
-      wptr<=wptr+(w_en & !full); // incr binary write pointer
+      w_ptr<=w_ptr+(w_en & !full); // incr binary write pointer
     end
   end
 
- always@(posedge wclk or negedge wrst_n) begin
+  always@(posedge w_clk or negedge wrst_n) begin
     if(!wrst_n) full <= 0;
     else        full <= (w_ptr[PTR_WIDTH-1:0]==r_ptr_sync[PTR_WIDTH-1:0] && w_ptr[PTR_WIDTH]!=r_ptr_sync[PTR_WIDTH]);
   end
